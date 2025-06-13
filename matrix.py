15f5f73190c10143ab6e25ef4a2a3a696e49802c
Jasper Ty <jasperty2@gmail.com>
2025-06-12 20:27:48 -0400

    Add new matrix object

---
+++ matrix.py
@@ -0,0 +1,179 @@
+"""
+Class representing a matrix with labeled rows and columns.
+Linear algebra capabilities are minimal.
+"""
+
+class Matrix:
+    """
+    An integer matrix class with arbitrary row and column indices.
+    """
+    def __init__(self, rows, cols, name=None, rows_name=None, cols_name=None):
+        self._data = dict()
+        if len(rows) == 0 or len(cols) == 0:
+            raise Exception("Empty matrix")
+        self._rows = rows.copy()
+        self._cols = cols.copy()
+        self._name = name
+        self._rows_name = rows_name
+        self._cols_name = cols_name
+
+        for row in rows:
+            for col in cols:
+                self._data[(row, col)] = 0
+
+    
+    def transpose(self):
+        out = Matrix(self.cols, self.rows)
+        for row in self.rows:
+            for col in self.cols:
+                out[col, row] = self[row, col]
+
+    
+    def identity(rows):
+        out = Matrix(rows, rows)
+        for row in rows:
+            out[(row, row)] = 1
+        return out
+
+
+    def set(self, row, col, val):
+        if (row, col) not in self:
+            raise KeyError(f"Index ({row}, {col}) does not exist in matrix.")
+        self._data[(row, col)] = val
+
+    
+    def get(self, row, col):
+        if (row, col) not in self:
+            raise KeyError(f"Index ({row}, {col}) does not exist in matrix.")
+        return self._data[(row, col)]
+
+
+    def __getitem__(self, key):
+        row, col = key
+        return self.get(row, col)
+
+    
+    def __setitem__(self, key, val):
+        row, col = key
+        return self.set(row, col, val)
+
+    
+    def __contains__(self, key):
+        row, col = key
+        return row in self._rows and col in self._cols
+
+
+    @property
+    def rows(self):
+        return self._rows.copy()
+
+    
+    @property
+    def cols(self):
+        return self._cols.copy()
+
+
+    def row(self, row):
+        for col in self.cols:
+            yield self[row, col]
+
+    
+    def col(self, col):
+        for row in self.rows:
+            yield self[row, col]
+
+    
+    def keys(self):
+        return iter(self._data.keys())
+
+
+    def __mul__(self, right):
+        if self.cols != right.rows:
+            raise KeyError("Incompatible matrix multiplication")
+
+        out = Matrix(self.rows, right.cols)
+        out._name = f"{self._name} * {right._name}"
+
+        for i in self.rows:
+            for j in right.cols:
+                for k in self.cols:
+                    out[i, j] += self[i, k] * right[k, j]
+        
+        return out
+
+    def __repr__(self) -> str:
+        """String representation."""
+        return f"Matrix({self.rows}, {self.cols})"
+
+
+    def __str__(self):
+        """Pretty string representation of the matrix."""
+        row_label_width = max(len(str(row)) for row in self.rows)
+        col_widths = [max(max(len(str(row)) for row in self.col(col)), len(str(col))) for col in self.cols]
+        total_width = 1 + (row_label_width + 2) + sum((width+3) for width in col_widths) + 1
+
+        top = "╭" + "─" * (row_label_width+2) + "┬" + "┬".join("─" * (width+2) for width in col_widths) + "╮\n"
+        bar = "├" + "─" * (row_label_width+2) + "┼" + "┴".join("─" * (width+2) for width in col_widths) + "┤\n"
+        mid = "├" + "─" * (row_label_width+2) + "┤" + "·".join(f" {'·':^{width}} " for width in col_widths) + "│\n"
+        bot = "╰" + "─" * (row_label_width+2) + "┴" + "─".join("─" * (width+2) for width in col_widths) + "╯"
+
+        out = ""
+        if self._name is not None:
+            out += f"{self._name:^{total_width}}\n"
+        out += top 
+        out += "│" + " " * (row_label_width+2) + "│" + "│".join(f" {str(col):^{col_width}} " for col, col_width in zip(self.cols, col_widths)) + "│\n"
+        out += bar
+
+        rows = self.rows
+        for i in range(len(rows)):
+            row = rows[i]
+            entries = self.row(row)
+            out += "│" + f" {str(row):^{row_label_width}} " + "│" + "·".join(f" {str(entry):^{col_width}} " for entry, col_width in zip(entries, col_widths)) + "│\n"
+            if i < len(rows) - 1:
+                out += mid
+            else:
+                out += bot
+
+        return out
+
+
+def row_swap(row_a, row_b, rows):
+    """
+    Constructs the elementary matrix which swaps two rows
+    """
+    out = Matrix(rows, rows, name=f"{row_a} ↔ {row_b}")
+
+    for row in rows:
+
+        if row == row_a:
+            out[(row, row_b)] = 1
+        elif row == row_b:
+            out[(row, row_a)] = 1
+        else:
+            out[(row, row)] = 1
+
+    return out
+
+
+def row_scale(row, scale, rows):
+    """
+    Constructs the elementary matrix which scales a row
+    """
+    out = Matrix.identity(rows)
+    out[(row, row)] *= scale
+
+    return out
+
+
+def row_sum(row_a, row_b, rows):
+    """
+    Constructs the elementary matrix which sums two rows
+    """
+    out = Matrix(rows, rows, name=f"{row_a} ← {row_a} + {row_b}")
+
+    for row in rows:
+        out[(row, row)] = 1
+        if row == row_a:
+            out[(row, row_b)] = 1
+
+    return out
